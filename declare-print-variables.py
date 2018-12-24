43ba1058c3d9ec7b256774a2f1d208cd27aa3b3e
sudhir.hanumanthappa <sudhih@gmail.com>
2018-12-24 22:31:54 +0530

    homework 1 from Variables

---
+++ declare-print-variables.py
@@ -0,0 +1,25 @@
+"""
+This program demonstrates variable declaration for various types of values
+"""
+
+# filename
+title = 'chandrashekara.mp3'
+# folder 
+album = 'Damaru'
+date = '2018-10-29'
+genre = 'devotion'
+date = '2018-10-29'
+track_number = 1
+publisher = 'Isha Foundation'
+language = 'en'
+frame_rate = 29.9
+codec = 'MPEG AAC Audio(mp4a)'
+
+print(title)
+print(album)
+print(date)
+print(track_number)
+print(publisher)
+print(language)
+print(frame_rate)
+print(codec)
